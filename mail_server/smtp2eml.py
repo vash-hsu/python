1543ab8b2bff3d7f3a1b2fa30c0db1459cade5b0
vash-hsu <vash.hsu@gmail.com>
2016-01-09 14:22:59 +0800

    support mail in mail extraction

--- mail_server/smtp2eml.py
+++ mail_server/smtp2eml.py
@@ -6,6 +6,8 @@ from SmtpServer import host_smtpd, Profile, whoami, debugger, logger
 import os
 import sys
 from functools import partial
+import email
+import mimetypes
 
 '''
 # porting from Perl to Python
@@ -16,14 +18,132 @@ Purpose
        Sub-Folder: SHA1(email address)
        each Email:
            YearMonthDay-HourMinuteSecond-SHA1.eml
+   --> save attachment into
+       YearMonthDay-HourMinuteSecond/MyFilename.MyExt
+   --> save attached RFC822 into
+       YearMonthDay-HourMinuteSecond/SHA1.eml
   (to do)
-   --> save attached RFC822
-       into YearMonthDay-HourMinuteSecond\00000001.eml
-       into YearMonthDay-HourMinuteSecond\00000002.eml
+   --> to support cross-platform under unit test cases
 '''
 
 
-Version = '20160107-v0.5'
+Version = '20160109-v0.6'
+
+
+def unicode_convert(buffer_src, charset_src, charset_dst):
+    try:
+        textdata = unicode(buffer_src, charset_src)
+        textdata = textdata.encode(charset_dst, 'ignore')
+    except UnicodeDecodeError:
+        debugger("ERROR: UnicodeDecodeError, covert from %s " % charset_src)
+        return ''
+    except UnicodeEncodeError:
+        debugger("ERROR: UnicodeDecodeError, covert to %s " % charset_dst)
+        return ''
+    return textdata
+
+
+def unicode_convert_safely(buffer_src, charset_src, charset_dst):
+    try:
+        return unicode_convert(buffer_src, charset_src, charset_dst)
+    except BaseException as err:
+        debugger("ERROR: Exception: %s" % str(err))
+        return ''
+
+
+class EMLReader:
+
+    def __init__(self, raw_string):
+        self._email = None
+        self._attachs = dict()
+        if raw_string and len(raw_string) > 0:
+            try:
+                self._email = email.message_from_string(raw_string)
+            except BaseException as err:
+                debugger("Exception: %s" % str(err))
+                self._email = None
+            if self._email:
+                self._retrieve_attachment()
+
+    @staticmethod
+    def _normalize_rfc822_entity(message):
+        if not message:
+            return ''
+        else:
+            try:
+                # debugger(repr(message))
+                terms = message.split('\n\n')  # how about platform dependent?
+                return '\n\n'.join(terms[1:])
+            except BaseException as err:
+                print "ERROR: %s" % str(err)
+                return ''
+
+    # package[payload_uuid] = (filename_candidate, payload)
+    def _parse_eml_to_name_payload(self, email_msg, package):
+        main_type = email_msg.get_content_maintype()
+        cont_type = email_msg.get_content_type()
+        # debugger("%s %s" % (main_type, cont_type))
+        if 'multipart' in main_type.lower():
+            return False
+        elif 'message/rfc822' == cont_type:
+            payload = self._normalize_rfc822_entity(email_msg.as_string())
+            # debugger("payload = %s" % repr(payload))
+            if payload:
+                payload_uuid = uuid(payload)
+                package[payload_uuid] = (payload_uuid + '.eml', payload)
+                return True
+        # i.e. text/html, text/plain which has no filename field
+        elif 'text' in main_type.lower() and \
+                '~oops~' == email_msg.get_filename('~oops~'):
+            return False
+        else:  # attachment
+            payload = email_msg.get_payload(None, True)
+            if not payload or len(payload) == 0:
+                return False
+            payload_uuid = uuid(payload)
+            # filename matter?
+            field_filename = email_msg.get_filename('')  # if fail, return ''
+            if len(field_filename) > 0:
+                if '=?' in field_filename and '?=' in field_filename:
+                    name = email.Utils.\
+                        collapse_rfc2231_value(field_filename).strip()
+                    (filename, charset) = email.Header.decode_header(name)[0]
+                    if charset and len(charset) > 0:
+                        field_filename = unicode_convert_safely(filename,
+                                                                charset,
+                                                                'utf-8')
+                field_filename = os.path.split(field_filename)[1]
+            else:
+                field_filename = payload_uuid
+            name, ext = os.path.splitext(field_filename)
+            if len(ext) == 0:
+                ext = mimetypes.guess_extension(cont_type)
+            if not ext or len(ext) < 2: # .exe, .js, .x
+                ext = '.bin'
+            filename_candidate = name + ext
+            package[payload_uuid] = (filename_candidate, payload)
+            return True
+        return False
+
+    def _retrieve_attachment(self):
+        if not self._email:
+            return False
+        if not self._email.is_multipart():
+            package = dict()
+            if self._parse_eml_to_name_payload(self._email, package):
+                self._attachs.update(package)
+        else:
+            for each_part in self._email.walk():
+                package = dict()
+                if self._parse_eml_to_name_payload(each_part, package):
+                    self._attachs.update(package)
+        return True
+
+    @property
+    def attachments(self):
+        for str_uuid in self._attachs.keys():
+            yield [str_uuid, ] + list(self._attachs[str_uuid])
+        pass
 
 
 class Profile4LocalStore(Profile):
@@ -38,20 +158,41 @@ class Profile4LocalStore(Profile):
         return self.config[whoami()]
 
 
+def write_file_to_disk(folder, filename, payload):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if os.path.isdir(folder):
+        target_path = os.path.join(folder, filename)
+        with open(target_path, 'wb') as writer:
+            writer.write(payload)
+    else:
+        debugger("ERROR: %s is not valid target folder" % folder)
+
+
+def write_file_to_disk_safely(folder, filename, payload):
+    try:
+        write_file_to_disk(folder, filename, payload)
+    except BaseException as err:
+        debugger("ERROR: Exception " + str(err))
+        return False
+    return True
+
+
 def write_mail_content_to_eml(folder_path, to_address, message):
-    message_id = "%s-%s" % (timestamp(), uuid(message))
+    ts = timestamp()
+    message_id = "%s-%s" % (ts, uuid(message))
+    # phase 1
     for i in to_address:
         # security issue if address give us surprise
         base_path = os.path.join(folder_path, uuid(i.lower()))
-        try:
-            if not os.path.exists(base_path):
-                os.makedirs(base_path)
-            if os.path.isdir(base_path):
-                eml_path = os.path.join(base_path, message_id + '.eml')
-                with open(eml_path, 'wb') as writer:
-                    writer.write(message)
-        except BaseException as err:
-            debugger("Exception: %s" % str(err))
+        write_file_to_disk_safely(base_path, message_id + '.eml', message)
+    # phase 2
+    if message:
+        email_reader = EMLReader(message)
+        for str_uuid, filename, payload in email_reader.attachments:
+            # debugger("process %s" % filename)
+            write_file_to_disk_safely(os.path.join(folder_path, ts),
+                                      filename, payload)
 
 
 if __name__ == "__main__":
